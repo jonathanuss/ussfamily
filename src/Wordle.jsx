b4dfa6d3ae7e62ece9718d1f8891b67d720d122a
Jonathan Uss <jonathan.uss@oracle.com>
2022-02-18 13:57:31 -0800

    Updated styles, added a spoil toggle and default masking of solutions for today and future dates.

--- src/Wordle.jsx
+++ src/Wordle.jsx
@@ -2,17 +2,19 @@ import React from "react";
 
 import Button from "react-bootstrap/Button";
 import Stack from "react-bootstrap/Stack";
+import ToggleButton from 'react-bootstrap/ToggleButton'
 
 import {wordList, oldWordList} from "./WordList.js";
 
 class Wordle extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { puzzleDate : new Date() };
+    this.state = { puzzleDate : new Date(), spoilFuture: false };
 
     this.setPrevious = this.setPrevious.bind(this);
     this.setNext = this.setNext.bind(this);
     this.setToday = this.setToday.bind(this);
+    this.toggleSpoilFuture = this.toggleSpoilFuture.bind(this);
   }
 
   boxify(text) {
@@ -45,6 +47,10 @@ class Wordle extends React.Component {
     this.setState( { puzzleDate: new Date() } );
   }
 
+  toggleSpoilFuture() {
+    this.setState( { spoilFuture: !this.state.spoilFuture });
+  }
+
   render() {
     const dateReference = new Date(2021, 5, 19, 0, 0, 0, 0);
     function getOffset(baseDate, currentDate) {
@@ -54,7 +60,14 @@ class Wordle extends React.Component {
         offsetDate.setHours(0, 0, 0, 0);
       return Math.round(timeDifference / 864e5);
     }
-    function getSolution(day, list) {
+    function getSolution(day, list, spoilFuture, disableSpoilToggle) {
+      // if the disableSpoilToggle is false it means the date is today or in the future
+      // in that case, if the spoilFuture toggle is false, return a masked value
+      if (!disableSpoilToggle && !spoilFuture) {
+        return "?????";
+      }
+
+      // it's either a past solution or they are okay with being spoiled
       const rawIndex = offsetForDate(day);
       const index = rawIndex % list.length;
       return list[index];
@@ -64,20 +77,37 @@ class Wordle extends React.Component {
     }
 
     const puzzleDate = this.state.puzzleDate;
-    const solution = getSolution(puzzleDate, wordList);
-    const oldSolution = getSolution(puzzleDate, oldWordList);
+    const spoilFuture = this.state.spoilFuture;
+    let disableSpoilToggle = false;
+    const today = new Date();
+
+    today.setHours(0, 0, 0, 0);
+    if (today.getTime() < puzzleDate.getTime()) {
+      disableSpoilToggle = false;
+    } else {
+      disableSpoilToggle = true;
+    }
+
+    const solution = getSolution(puzzleDate, wordList, spoilFuture, disableSpoilToggle);
+    const oldSolution = getSolution(puzzleDate, oldWordList, spoilFuture, disableSpoilToggle);
     const dayOffset = offsetForDate(puzzleDate);
 
     return (
       <Stack fluid direction='vertical'>
         <p>Wordle {dayOffset}</p>
-        <div>(NYT Version Solution): {this.boxify(solution)}</div>
+        <div className="diminished">(NYT Version Solution):</div>
+        <div className="solution">{this.boxify(solution)}</div>
         <div className="diminished">(Original Wordle Solution): {oldSolution}</div>
-        <p>{puzzleDate.toLocaleDateString("en-US", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</p>
+        <div>{puzzleDate.toLocaleDateString("en-US", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</div>
         <div>
           <Button variant="info" key="prev" className="buttons" onClick={this.setPrevious}>-</Button>
           <Button variant="info" key="today" className="buttons" onClick={this.setToday}>Today</Button>
           <Button variant="info" key="next" className="buttons" onClick={this.setNext}>+</Button>
+        </div>
+        <div>
+          <ToggleButton id="toggle-check" type="checkbox" variant="outline-danger" checked={spoilFuture} value="1" onChange={this.toggleSpoilFuture} disabled={disableSpoilToggle}>
+            Spoil It
+          </ToggleButton>
         </div>
       </Stack>
     );
